c68a7250a293b7cced781e2d85a311256aa6de8c
Manhcodee <phammanh2952@gmail.com>
2024-11-11 16:35:03 +0700

    fix mini test

--- src/Main.java
+++ src/Main.java
@@ -3,57 +3,41 @@
 public class Main {
     public static void main(String[] args) {
         //Nhập số sách
-        ProgrammingBook[] programmingBooks = {
-                new ProgrammingBook("pb01", "JavaScript", 100, "Autor 1", "JavaScript", "Spring"),
-                new ProgrammingBook("pb02", "Java", 120, "Autor 2", "Java", "Reach"),
-                new ProgrammingBook("pb03", "Python", 90, "Autor 3", "PHP", "Django")
-        };
+        Book[] books = new Book[6];
+        books[0] = new ProgrammingBook("pb01", "JavaScript", 100, "Autor 1", "JavaScript", "Spring");
+        books[1] = new ProgrammingBook("pb02", "Java", 120, "Autor 2", "Java", "Reach");
+        books[2] = new ProgrammingBook("pb03", "Python", 90, "Autor 3", "PHP", "Django");
+        books[3] = new FictionBook("F001", "OnePice", 180, "Author 4", "Viễn tưởng 1");
+        books[4] = new FictionBook("F002", "Naruto", 85, "Author 5", "Viễn tưởng 2");
+        books[5] = new FictionBook("F003", "Dragonball", 80, "Author 6", "Viễn tưởng 3");
 
-        FictionBook[] fictionBooks = {
-                new FictionBook("F001", "OnePice", 180, "Author 4", "Viễn tưởng 1"),
-                new FictionBook("F002", "Naruto", 85, "Author 5", "Viễn tưởng 2"),
-                new FictionBook("F003", "Dragonball", 80, "Author 6", "Viễn tưởng 3")
-        };
-
-        //Tính tổng
         int totalPrice = 0;
-        for (ProgrammingBook pBook : programmingBooks) {
-            totalPrice += pBook.getPrice();
-        }
-        for (FictionBook fBook : fictionBooks) {
-            totalPrice += fBook.getPrice();
+        int findJava = 0;
+        int findCategory1 = 0;
+        int priceFiction = 0;
+
+        for (Book book : books) {
+            totalPrice += book.getPrice();
+            if (book instanceof ProgrammingBook) {
+                if (((ProgrammingBook) book).getLanguage().equals("Java")) {
+                    findJava++;
+                }
+            }
+            if (book instanceof FictionBook) {
+                if (((FictionBook) book).getCategory().equals("Viễn tưởng 1")) {
+                    findCategory1++;
+                }
+            }
+            if (book instanceof FictionBook) {
+                if (((FictionBook) book).getPrice() < 100) {
+                    priceFiction++;
+                }
+            }
         }
 
         System.out.println("Tong tien cua 6 cuon sach sau giam gia la: " + totalPrice);
-
-        //Đếm số sách có language Java
-        int findJava = 0;
-        for (ProgrammingBook pBook : programmingBooks) {
-            if (pBook.getLanguage().equals("Java")) {
-                findJava++;
-            }
-        }
-
         System.out.println("so sach language Java la: " + findJava);
-
-        //Đếm số sách có category là "Viễn tưởng 1"
-        int findCategory1 = 0;
-        for (FictionBook fBook : fictionBooks) {
-            if (fBook.getCategory().equals("Viễn tưởng 1")) {
-                findCategory1++;
-            }
-        }
-
         System.out.println("So sach Vien tuong 1 la: " + findCategory1);
-
-        //Đếm số sách fiction có giá nhỏ hơn 100
-        int priceFiction = 0;
-        for (FictionBook fBook : fictionBooks) {
-            if (fBook.getPrice() < 100) {
-                priceFiction++;
-            }
-        }
-
-        System.out.println("so sach fiction gia < 100 la: " +priceFiction);
+        System.out.println("so sach fiction gia < 100 la: " + priceFiction);
     }
 }
